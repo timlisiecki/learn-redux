b7562e284b9141d65d1017310c3130ce322b22d9
Tim <tim.lisiecki@gmail.com>
2017-05-01 11:01:18 -0400

    break up reducer into multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -10,52 +10,58 @@ var stateDefault = {
 };
 var nextHobbyId = 1;
 var nextMovieId = 1;
-var reducer = (state = stateDefault, action) => {
-	// state = state || {name: "Anonymous"}; >>>> this is shortened in above argument using ES6
 
+var nameReducer = (state = "Anonymous", action) => {
 	switch (action.type) {
 		case "CHANGE_NAME":
-			return {
-				...state,
-				name: action.name
-			};
+			return action.name;
+		default:
+			return state;
+	};
+};
+
+var hobbiesReducer = (state = [], action) => {
+	switch (action.type) {
 		case "ADD_HOBBY":
-			return {
+			return [
 				...state,
-				hobbies: [
-					...state.hobbies,
-					{
-						id: nextHobbyId++,
-						hobby: action.hobby
-					}
-				]
-			};
+				{
+					id: nextHobbyId++,
+					hobby: action.hobby
+				}
+			];
 		case "REMOVE_HOBBY":
-			return {
-				...state,
-				hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-			};
+			return state.filter((hobby) => hobby.id !== action.id);
+		default:
+			return state;
+
+	}
+};
+
+var moviesReducer = (state = [], action) => {
+	switch (action.type) {
 		case "ADD_MOVIE":
-			return {
+			return [
 				...state,
-				movies: [
-					...state.movies,
-					{
-						id: nextMovieId++,
-						title: action.title,
-						genre: action.genre
-					}
-				]
-			};
+				{
+					id: nextMovieId++,
+					title: action.title,
+					genre: action.genre
+				}
+			];
 		case "REMOVE_MOVIE":
-			return {
-				...state,
-				movies: state.movies.filter((movie) => movie.id !== action.id)
-			};
+			return state.filter((movie) => movie.id !== action.id);
 		default:
 			return state;
 	}
 };
+
+var reducer = redux.combineReducers({
+	name: nameReducer,
+	hobbies: hobbiesReducer,
+	movies: moviesReducer
+});
+
 var store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
